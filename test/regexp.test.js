4c97c392e0e7b6bbb3491564d09d4c9f2ae3ad4b
Anthony Ffrench <tffrench@us.ibm.com>
2016-04-27 14:29:17 -0400

    Fix ,  and  operators to align with the CQ syntax. closes #12

---
+++ test/regexp.test.js
@@ -0,0 +1,51 @@
+var should = require('should');
+describe('cloudant regexp', function() {
+  var Foo;
+  var N = 10;
+  before(function(done) {
+    require('./init.js');
+    db = getSchema();
+    Foo = db.define('Foo', {
+      bar: {type: String, index: true}
+    });
+    db.automigrate(done);
+  });
+  it('create some foo', function(done) {
+    var foos = Array.apply(null, {length: N}).map(function(n, i) {
+      return {bar: String.fromCharCode(97+i)};
+    });
+    Foo.create(foos, function(err, entries) {
+      should.not.exist(err);
+      entries.should.have.lengthOf(N);
+      done();
+    });
+  });
+  it('find all foos begining with b', function(done) {
+    Foo.find({where: {bar: {regexp: '^b'}}}, function(err, entries) {
+      console.log (entries);
+      entries.should.have.lengthOf(1);
+      entries[0].bar.should.equal('b');
+      done();
+    });
+  });
+  it('find all foos like b', function(done) {
+    Foo.find({where: {bar: {like: 'b'}}}, function(err, entries) {
+      console.log (entries);
+      entries.should.have.lengthOf(1);
+      entries[0].bar.should.equal('b');
+      done();
+    });
+  });
+  it('find all foos not like b', function(done) {
+    Foo.find({where: {bar: {nlike: 'b'}}}, function(err, entries) {
+      console.log (entries);
+      entries.should.have.lengthOf(N-1);
+      done();
+    });
+  });
+  after (function(done) {
+    Foo.destroyAll(function() {
+      done();
+    });
+  });
+});
