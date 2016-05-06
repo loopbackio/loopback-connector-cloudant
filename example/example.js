fc458cb8171b15da15a661c39f1a1ce7b8466015
Ryan Graham <r.m.graham@gmail.com>
2016-05-06 16:38:46 -0700

    update copyright notices and license

--- example/example.js
+++ example/example.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2015,2016. All Rights Reserved.
+// Node module: loopback-connector-cloudant
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
 var DataSource = require('loopback-datasource-juggler').DataSource;
 var Cloudant = require('../'); // loopback-connector-cloudant
 
