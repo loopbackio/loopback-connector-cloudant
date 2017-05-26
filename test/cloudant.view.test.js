606b8ddb1c2ea8cc379b73612ad86f37640154ab
jannyHou <juehou@ca.ibm.com>
2017-05-26 16:19:39 -0400

    feedback

--- test/cloudant.view.test.js
+++ test/cloudant.view.test.js
@@ -1,4 +1,4 @@
-// Copyright IBM Corp. 2016. All Rights Reserved.
+// Copyright IBM Corp. 2017. All Rights Reserved.
 // Node module: loopback-connector-cloudant
 // This file is licensed under the Artistic License 2.0.
 // License text available at https://opensource.org/licenses/Artistic-2.0
@@ -6,92 +6,99 @@
 'use strict';
 
 require('./init.js');
-var Cloudant = require('../lib/cloudant');
 var _ = require('lodash');
-var url = require('url');
-var should = require('should');
 var async = require('async');
-var db, TestView, Random, samples;
+var should = require('should');
+var url = require('url');
 
-describe('cloudant view', function() {
+var db, sampleData;
+
+describe.only('cloudant view', function() {
   describe('viewDocs', function(done) {
     before(function(done) {
       db = getDataSource();
-      TestView = db.define('TestView', {
-        name: {type: String},
-      }, {forceId: false});
-      Random = db.define('Random', {
-        foo: {type: String},
-      }, {forceId: false});
+      var connector = db.connector;
       db.on('connected', function() {
-        async.series([createModelTestView, seedData, insertViewDdoc], done);
+        async.series([insertSampleData, insertViewDdoc], done);
       });
 
-      function createModelTestView(cb) {
-        db.automigrate(cb);
-      };
-      function seedData(cb) {
-        samples = [{name: 'John'}, {name: 'Tom'}, {name: 'Zoe'}];
-        var randomSamples = [{foo: 'bar1'}, {foo: 'bar2'}];
-        TestView.create(samples, function(err) {
-          if (err) return cb(err);
-          Random.create(randomSamples, cb);
-        });
+      function insertSampleData(cb) {
+        sampleData = generateSamples();
+        connector.cloudant.use(connector.getDbName(connector))
+          .bulk({docs: sampleData}, cb);
       };
       function insertViewDdoc(cb) {
-        // The design doc must be stored with double quote,
-        // and map function cannot join by '+'
-        /* eslint-disable */
-        var ddoc = {
-          "_id": "_design/getModel",
-          "views": {
-            "returnModelInstances": {
-              "map": "function(doc) { if(doc.loopback__model__name) { emit(doc.loopback__model__name, doc); }}"
-            }
+        var viewFunction = function(doc) {
+          if (doc.model) {
+            emit(doc.model, doc);
           }
         };
-        /* eslint-enable */
-        // We test everything in a single db, so the view ddoc
-        // and model 'TestView' should be in same db.
-        db.connector.selectModel('TestView').db.insert(ddoc, cb);
+        var ddoc = {
+          _id: '_design/model',
+          views: {
+            getModel: {
+              map: viewFunction.toString(),
+            },
+          },
+        };
+        connector.cloudant.use(connector.getDbName(connector)).insert(
+          JSON.parse(JSON.stringify(ddoc)), cb);
       };
     });
-    after(function(done) {
-      TestView.destroyAll(function(err) {
-        if (err) return done(err);
-        Random.destroyAll(done);
-      });
-    });
 
-    it('returns result by query a view', function(done) {
-      db.connector.viewDocs('getModel', 'returnModelInstances',
+    it('returns result by quering a view', function(done) {
+      db.connector.viewDocs('model', 'getModel',
         function(err, results) {
-          results.total_rows.should.equal(5);
-          results.rows.forEach(isLBModelInstance);
+          results.total_rows.should.equal(4);
+          results.rows.forEach(hasModelName);
           done(err);
 
-          function isLBModelInstance(elem) {
-            elem.value.hasOwnProperty('loopback__model__name').
+          function hasModelName(elem) {
+            elem.value.hasOwnProperty('model').
               should.equal(true);
           };
         });
     });
 
     it('queries a view with key filter', function(done) {
-      db.connector.viewDocs('getModel', 'returnModelInstances', {
-        'key': 'TestView',
+      db.connector.viewDocs('model', 'getModel', {
+        'key': 'customer',
       }, function(err, results) {
-        var expectedNames = _.map(samples, function(obj) {
-          return obj.name;
-        });
-        results.rows.forEach(isTestViewInstance);
+        var expectedNames = ['Zoe', 'Jack'];
+        results.rows.forEach(belongsToModelCustomer);
         done(err);
 
-        function isTestViewInstance(elem) {
-          elem.key.should.equal('TestView');
+        function belongsToModelCustomer(elem) {
+          elem.key.should.equal('customer');
           _.indexOf(expectedNames, elem.value.name).should.not.equal(-1);
         }
       });
     });
   });
 });
+
+function generateSamples() {
+  var samples = [
+    {
+      model: 'purchase',
+      'customer_id': 1,
+      basket: ['food', 'drink'],
+    },
+    {
+      model: 'purchase',
+      'customer_id': 2,
+      basket: ['book', 'video'],
+    },
+    {
+      model: 'customer',
+      customerId: 1,
+      name: 'Zoe',
+    },
+    {
+      model: 'customer',
+      customerId: 2,
+      name: 'Jack',
+    },
+  ];
+  return samples;
+}
