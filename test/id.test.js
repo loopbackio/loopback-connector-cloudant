27598ce399ed339697e4cfc4615c7e959c2f6c8f
Taranveer Virk <taranveer@virk.cc>
2017-07-26 12:08:40 -0400

    Ensure _id can be set as id on a model
    Fixes an issue where if _id was set as id on model, any query won’t
    return _id anymore.

---
+++ test/id.test.js
@@ -0,0 +1,52 @@
+// Copyright IBM Corp. 2016. All Rights Reserved.
+// Node module: loopback-connector-cloudant
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
+'use strict';
+
+var Cloudant = require('../lib/cloudant');
+var should = require('should');
+var db, Task;
+
+describe('using _id as id', function() {
+  before(function(done) {
+    db = getDataSource();
+
+    Task = db.define('Task', {
+      task: {type: String},
+      owner: {type: String},
+      _id: {type: String, id: true},
+    }, {forceId: true});
+
+    db.automigrate('Task', done);
+  });
+
+  after(function(done) {
+    Task.destroyAll(done);
+  });
+
+  it('result should include _id', function(done) {
+    var ownerName = 'John Smith';
+    Task.create({
+      task: 'some random task',
+      owner: ownerName,
+    }, function(err, task) {
+      if (err) return done(err);
+
+      should.exist(task);
+      should.exist(task._id);
+
+      var taskId = task._id;
+      Task.find({where: {owner: ownerName}}, function(err, tasks) {
+        if (err) return done(err);
+
+        tasks.should.have.lengthOf(1);
+        var task = tasks[0];
+        task.should.have.property('_id');
+        task._id.should.equal(taskId);
+        done();
+      });
+    });
+  });
+});
